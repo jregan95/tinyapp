24e8c6316bf9023e67d10036fa1f85c1b30b40ff
Jill Regan <jillian.jnr@hotmail.com>
2023-11-27 16:11:54 -0500

    when aparamter is entered into URL it will give the paramater and the long URL

--- express_server.js
+++ express_server.js
@@ -10,8 +10,14 @@ const urlDatabase = {
 };
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
+  const urlsTable = { urls: urlDatabase };
+  res.render("urls_index", urlsTable);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const id = req.params.id
+  const logID = { id: id, longURL: urlDatabase[id]};
+  res.render("urls_show", logID);
 });
 
 app.get('/', (req, res) => {
